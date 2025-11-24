72d05662ae6581b4f2c6d381305dd38e9a8eaaf3
SusmitDeshpande <satejsdeshpande@gmail.com>
2025-11-24 12:20:31 +0530

    Basic summarization is done

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+from langchain_classic.chains.summarize import load_summarize_chain
 
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -17,11 +18,12 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 load_dotenv()
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY") 
 
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = GoogleGenerativeAI(model="gemini-2.5-flash")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 
-retriever = WikipediaRetriever(load_max_docs=5)
+retriever = WikipediaRetriever(load_max_docs=1)
 
 template = """Use the following pieces of retrieved context to answer the question. 
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -45,9 +47,9 @@ choice = st.radio(
 
 if choice=='yes':
     user_info = st.text_input("Please enter your information here : ")
-    user_doc = Document(page_content=user_info, metadata={"source":"user provided info"})
+    user_doc = Document(page_content=user_info, metadata={"source":"user provided info"}) 
 
-
+final_summary = None
 
 if st.button("Prep"):
     wiki_docs = retriever.invoke(topic)
@@ -58,13 +60,23 @@ if st.button("Prep"):
         all_docs = wiki_docs
     
     split_docs = text_splitter.split_documents(all_docs)
-    
+
     db = FAISS.from_documents(split_docs, embeddings)
 
     # docs_with_scores = db.similarity_search_with_score(question)
 
     retriever = db.as_retriever()
+        
+    summarize_chain = load_summarize_chain(
+    llm,
+    chain_type="map_reduce",
+    verbose=True # helpful for tracking the recursive steps
+    )
 
+    final_summary = summarize_chain.run(all_docs)
+    
+st.write(final_summary)
+    
 rag_chain = (
 {"context": retriever | format_docs, "question": RunnablePassthrough()}
 | prompt
